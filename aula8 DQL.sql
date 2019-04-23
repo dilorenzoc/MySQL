e4a4e67ecfed8cc760af2118c325e80a7985551b
Camila Di Lorenzo <43864961+dilorenzoc@users.noreply.github.com>
2019-04-22 21:49:00 -0300

    Add files via upload

--- aula8 DQL.sql
+++ aula8 DQL.sql
@@ -39,3 +39,33 @@ select codigo, nome from Funcionario order by nome asc;
 select codigo, nome from Funcionario order by nome;
 
 select codigo, nome from Funcionario order by nome desc;
+
+select Codigo, Nome from Cliente where Codigo in ( select Cliente from Pedido where Valor >= 2000 );
+
+select a.Codigo, a.Nome, b.Setor from Funcionario a inner join Setor b on (b.Codigo = a.Setor);
+
+select a.Codigo, a.Nome, b.Setor from Funcionario a left outer join Setor b on (b.Codigo = a.Setor);
+
+select a.Codigo, a.Nome, b.Setor from Funcionario a right outer join Setor b on (b.Codigo = a.Setor);
+
+select Nome from Funcionario union select Nome from Cliente;
+
+select Nome from Funcionario union all select Nome from Cliente;
+
+select Codigo, Nome from Cliente where Codigo in ( select Cliente from Pedido );
+
+select avg(Salario) as Media from Funcionario;
+
+select max(Codigo) as Maior_Codigo from Funcionario;
+
+select min(Codigo) as Menor_Codigo from Funcionario;
+
+select sum(Salario) as Total from Funcionario;
+
+select count(Codigo) as QtTotal from Funcionario;
+
+select distinct (Cliente) from Pedido;
+
+select sum(Valor) as Soma, Cliente from Pedido group by Cliente;
+
+select * from pedido;
